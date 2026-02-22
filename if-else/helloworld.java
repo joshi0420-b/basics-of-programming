5804602f097efca31ccb3170ce5fa5916c40af3b
joshi0420-b <bhupeshjoshi0420@gmail.com>
2026-02-22 20:30:59 +0530

    Delete if-else/helloworld.java

--- if-else/helloworld.java
+++
@@ -1,8 +0,0 @@
-public class helloworld {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello Bhupesh joshi");
-        System.out.println("hello how are you ");
-        System.out.println("*\n**\n***\n****");
-    }
-}
